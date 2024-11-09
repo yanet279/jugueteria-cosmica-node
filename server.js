e15005985860446a06417b0fc6d15b932712fab6
Yanet Choque <sol3127g@gmaill.com>
2024-11-08 22:14:24 -0300

    Actualización del proyecto.

--- server.js
+++ server.js
@@ -7,13 +7,22 @@ import DBMongoDB from './model/DBMongoDB.js';
 const app = express();
 
 app.use(express.static('public', { extensions: ['html', 'htm'] }));
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Caso de uso Products
 app.use('/api/products', routerProducts);
 
+// Ruta para el index.html en la raíz
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
 
+// Configuración del puerto y lanzamiento del servidor
 const PORT = config.PORT;
-const server = app.listen(PORT, () => console.log(`Servidor Express escuchando en el puerto ${PORT}.`));
-server.on('error', error => console.log('Error al iniciar el servidor Express: ' + error.message));
+const server = app.listen(PORT, () => 
+    console.log(`Servidor Express escuchando en el puerto ${PORT}.`)
+);
+server.on('error', error => 
+    console.log('Error al iniciar el servidor Express: ' + error.message)
+);
